31e36b1a487ccfcac2d1c874fda1fe64e4e44e69
anbuchelva <anbuchelva@gmail.com>
2024-08-15 22:31:26 +0530

    update keyboard.js with latest changes

--- src/keyboard.js
+++ src/keyboard.js
@@ -3,15 +3,17 @@ var chartsDailyKeyboard = {
     [
       { text: '➡️ Range', callback_data: 'distance_vs_range' },
       { text: '🎯 Efficiency', callback_data: 'distance_vs_efficiency' },
-      { text: '🔋 SOC%/Km', callback_data: 'distance_vs_battery_km' },
-    ],
-    [
-      { text: '🪫 SOC%/Day', callback_data: 'battery_per_day' },
-      { text: '🚀 Speed', callback_data: 'top_vs_avg_speed' },
-      { text: '💰 Savings', callback_data: 'savings' },
-    ],
-    [
       { text: '🏁 Ride Pattern', callback_data: 'ride_pattern' },
+
+    ],
+    [
+      { text: '🔋 SOC%/Km', callback_data: 'distance_vs_battery_km' },
+      { text: '🪫 SOC% vs Km', callback_data: 'battery_per_day' },
+      { text: '🪫 SOC Usage', callback_data: 'battery_drain_day' },
+    ],
+    [
+      { text: '💰 Savings', callback_data: 'savings' },
+      { text: '🚀 Speed', callback_data: 'top_vs_avg_speed' },
       { text: '📯 Horn', callback_data: 'horn_count' },
     ],
     [
@@ -27,15 +29,16 @@ var chartsWeeklyKeyboard = {
     [
       { text: '➡️ Range', callback_data: 'distance_vs_range_wk' },
       { text: '🎯 Efficiency', callback_data: 'distance_vs_efficiency_wk' },
-      { text: '🔋 SOC%/Km', callback_data: 'distance_vs_battery_km_wk' },
-    ],
-    [
-      { text: '🪫 SOC%/Wk', callback_data: 'battery_per_wk' },
-      { text: '🚀 Speed', callback_data: 'top_vs_avg_speed_wk' },
-      { text: '💰 Savings', callback_data: 'savings_wk' },
-    ],
-    [
       { text: '🏁 Ride Pattern', callback_data: 'ride_pattern_wk' },
+    ],
+    [
+      { text: '🔋 SOC%/Km', callback_data: 'distance_vs_battery_km_wk' },
+      { text: '🪫 SOC% vs KM', callback_data: 'battery_per_wk' },
+      { text: '🪫 SOC Usage', callback_data: 'battery_drain_wk' },
+    ],
+    [
+      { text: '💰 Savings', callback_data: 'savings_wk' },
+      { text: '🚀 Speed', callback_data: 'top_vs_avg_speed_wk' },
       { text: '📯 Horn', callback_data: 'horn_count_wk' },
     ],
     [
@@ -51,15 +54,18 @@ var chartsMonthlyKeyboard = {
     [
       { text: '➡️ Range', callback_data: 'distance_vs_range_mon' },
       { text: '🎯 Efficiency', callback_data: 'distance_vs_efficiency_mon' },
-      { text: '🔋 SOC%/Km', callback_data: 'distance_vs_battery_km_mon' },
-    ],
-    [
-      { text: '🪫 SOC%/Mon', callback_data: 'battery_per_mon' },
-      { text: '🚀 Speed', callback_data: 'top_vs_avg_speed_mon' },
-      { text: '💰 Savings', callback_data: 'savings_mon' },
-    ],
-    [
       { text: '🏁 Ride Pattern', callback_data: 'ride_pattern_mon' },
+    ],
+    [
+      { text: '🔋 SOC%/Km', callback_data: 'distance_vs_battery_km_mon' },
+      { text: '🪫 SOC% vs Km', callback_data: 'battery_per_mon' },
+      { text: '🪫 SOC Usage', callback_data: 'battery_drain_mon' },
+
+
+    ],
+    [
+      { text: '💰 Savings', callback_data: 'savings_mon' },
+      { text: '🚀 Speed', callback_data: 'top_vs_avg_speed_mon' },
       { text: '📯 Horn', callback_data: 'horn_count_mon' },
     ],
     [
@@ -69,3 +75,12 @@ var chartsMonthlyKeyboard = {
     ],
   ],
 };
+
+var batteryAlertKeyboard = {
+  inline_keyboard: [
+    [
+      { text: '🪫 Set Alert %', callback_data: 'set_battery_alert_pct' },
+      { text: '⏩ Skip this time', callback_data: 'skip_battery_alert' },
+    ]
+  ]
+}
