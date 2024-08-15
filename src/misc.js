565ac4ab568a8026527a3c88bd2e2a0484884aa9
anbuchelva <anbuchelva@gmail.com>
2024-08-15 22:32:32 +0530

    update misc.js with latest changes

--- src/misc.js
+++ src/misc.js
@@ -34,5 +34,57 @@ function updatedScriptProperties() {
             PropertiesService.getScriptProperties().setProperty(key, value);
         }
     }
-    logMessage('Script Properties are updated!');
+    Logger.log('Script Properties are updated!')
+    deleteWebhook()
+    setWebhook();
+}
+
+function batteryAlert(soc) {
+    var batteryAlertCapacity = getBatteryAlertCapacity()
+    var batteryAlert = getBatteryAlert()
+    if (soc < batteryAlertCapacity && batteryAlert == 'true') {
+        sendToTelegram(ADMIN, '⚠️ <b>Low Battery Alert</b> ⚠️\n\nYour current SOC is ' + soc + '%.\nYour Target is set to ' + batteryAlertCapacity + '%.\n\nEnsure to recharge your vehicle to prevent any unexpected situations.', batteryAlertKeyboard)
+    }
+}
+
+function getBatteryAlertCapacity() {
+    var BATTERY_ALERT_CAPACITY = USER_PROPERTIES.getProperty('BATTERY_ALERT_CAPACITY');
+    if (BATTERY_ALERT_CAPACITY === null) {
+        BATTERY_ALERT_CAPACITY = 30;
+        USER_PROPERTIES.setProperty('BATTERY_ALERT_CAPACITY', BATTERY_ALERT_CAPACITY);
+    } else {
+        BATTERY_ALERT_CAPACITY = parseInt(BATTERY_ALERT_CAPACITY);
+    }
+    setBatteryAlert('true')
+    return BATTERY_ALERT_CAPACITY;
+}
+
+function getBatteryAlert() {
+    var BATTERY_ALERT = USER_PROPERTIES.getProperty('BATTERY_ALERT');
+    if (BATTERY_ALERT === null) {
+        BATTERY_ALERT = 'true';
+        USER_PROPERTIES.setProperty('BATTERY_ALERT', BATTERY_ALERT);
+    } else {
+        BATTERY_ALERT = BATTERY_ALERT;
+    }
+    return BATTERY_ALERT;
+}
+
+function setBatteryAlertCapacity(value) {
+    PropertiesService.getScriptProperties().setProperty('BATTERY_ALERT_CAPACITY', value);
+    setBatteryAlert('true');
+}
+
+function setBatteryAlert(value) {
+    PropertiesService.getScriptProperties().setProperty('BATTERY_ALERT', value);
+    if (value == 'false') {
+        sendToTelegram(ADMIN, "✅ <b>Low Battery Alert - Disabled</b> ✅\n\nThe low battery alert is disabled for now.  However it will be enabled automatically when you take a next ride.")
+    }
+}
+
+function convertEpochToIST(epohTime) {
+    // var epohTime= 1718546437556
+    var istTime = Utilities.formatDate(new Date(epohTime), "Asia/Kolkata", "yyyy-MM-dd HH:mm:ss");
+    Logger.log(istTime)
+    return istTime;
 }
